8fa8b0a47d5bda26e776938703c655d5cf5b640e
Doodles3 <eduardomoraes84@yahoo.com.br>
2020-12-18 20:29:10 -0300

    Create a header component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function About() {
   return (
     <div style={{ color: `teal` }}>
-      <h1>About Gatsby</h1>
+      <Header headerText="About Gatsby" />
       <p>Such wow. Very React.</p>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello Gatsby!</h1>
+      <Header headerText="Hello Gatsby!" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
